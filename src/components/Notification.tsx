fe23ce45c7664299e9eab4662a7b156d066559eb
janr0599 <javiernr0599@gmail.com>
2024-10-06 22:05:53 -0400

    add lazy to index page

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -41,7 +41,7 @@ export default function Notification() {
                                 </div>
                                 <div className="ml-3 w-0 flex-1 pt-0.5">
                                     <p className="text-sm font-medium text-gray-900">
-                                        Notificación
+                                        Notification
                                     </p>
                                     <p className="mt-1 text-sm text-gray-500">
                                         {notification.text}
--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,27 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import IndexPage from "./views/IndexPage";
 import Layout from "./layouts/Layout";
 
+const IndexPage = lazy(() => import("./views/IndexPage"));
 const FavoritesPage = lazy(() => import("./views/FavoritesPage"));
 function Approuter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<Layout />}>
-                    <Route path="/" element={<IndexPage />} index />
+                    <Route
+                        path="/"
+                        element={
+                            <Suspense fallback="Loading...">
+                                <IndexPage />
+                            </Suspense>
+                        }
+                        index
+                    />
                     <Route
                         path="/favorites"
                         element={
-                            <Suspense fallback="cargando...">
+                            <Suspense fallback="Loading...">
                                 <FavoritesPage />
                             </Suspense>
                         }
